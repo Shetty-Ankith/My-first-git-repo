47273e45927e7c731d1ba4b83ac48276de574cbd
Shetty-Ankith <ankith.shetty@codematrix.org>
2025-10-03 19:05:32 +0530

    Added division function

--- src/App.js
+++ src/App.js
@@ -94,6 +94,17 @@ function subtract(a, b) {
   return a - b;
 }
 
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error("Cannot divide by zero");
+  }
+  return a / b;
+}
+
 	return (
 		<ColorSchemeProvider
 			colorScheme={colorScheme}
